8a673f7f2f20b66197970bafbfb5b33960505385
yousseftange17 <46229103+yousseftange17@users.noreply.github.com>
2019-01-18 12:37:03 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const adminprefix = "!";
-const developers = ["456919044663279627" , "474200581163057153"];
+const developers = ["339035485420912641" , "525749212198207508"];
 
 
 client.on('ready', () => {
